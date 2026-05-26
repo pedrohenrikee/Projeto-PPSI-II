2f9da1ca6b56d1bcf18eab067d92270c00c4afe8
Vinícius Faria <64180986+ViniciusFaria-png@users.noreply.github.com>
2026-05-26 14:57:35 -0300

    adiciona logo e reorganiza estilos globais

--- js/components/header.js
+++ js/components/header.js
@@ -3,10 +3,11 @@ class MainHeader extends HTMLElement {
     const inPages = window.location.pathname.includes('/pages/');
     const prefix = inPages ? '../' : './pages/';
     const prefixIndex = inPages ? '../../index.html' : './index.html';
+    const logoSrc = inPages ? '../../assets/logo.png' : './assets/logo.png';
 
     this.innerHTML = `
       <header>
-        <div class="logo" style="font-weight: bold; color: #0a3d62;">World Micro</div>
+        <img src="${logoSrc}" alt="World Micro Logo" style="height: 84px; width: auto; object-fit: contain; margin-right: 10px;">
         <nav>
           <a href="${prefixIndex}">Home</a>
           <a href="${prefix}store/store.html">Loja</a>
